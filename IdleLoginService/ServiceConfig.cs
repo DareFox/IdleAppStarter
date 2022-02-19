b8f52a2c14b6cc9a89b2c8500e59f81f35fecd4a
DareFox <easymalink@gmail.com>
2022-02-19 20:18:11 +0300

    Add shortcut for timeout argument

--- IdleLoginService/ServiceConfig.cs
+++ IdleLoginService/ServiceConfig.cs
@@ -5,7 +5,7 @@ namespace IdleSharedLib
 {
     public class ServiceConfig : Config
     {
-        [Option("timeout", Default = (long)45000, HelpText = "Connection timeout between client and server in milliseconds")]
+        [Option('t', "timeout", Default = (long)45000, HelpText = "Connection timeout between client and server in milliseconds")]
         public long connectionTimeoutMS { get; set; }
     }
 }
