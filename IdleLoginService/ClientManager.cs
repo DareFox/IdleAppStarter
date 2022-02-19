b2b28499af210676dc7e65c807ea860080bf9a95
DareFox <easymalink@gmail.com>
2022-02-19 14:45:31 +0300

    Create Client side manager
    Created this manager to seperate service logic from client creation and managment logic

---
+++ IdleLoginService/ClientManager.cs
@@ -0,0 +1,111 @@
+﻿using IdleSharedLib;
+using IdleSharedLib.WinAPI;
+using Newtonsoft.Json;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using ZetaIpc.Runtime.Server;
+using static IdleSharedLib.WinAPI.ProcessExtensions;
+
+namespace IdleLoginService
+{
+    class ClientManager
+    {
+        // Program arguments
+        private int port;
+        private long idleTime;
+        private List<string> executables;
+
+        private Logger logger = LogManager.GetCurrentClassLogger();
+        private int timeoutMS = 45000; // in milliseconds, 1000ms = 1sec
+        private IpcServer server;
+
+        // Process control
+        private Nullable<int> clientID = 0;
+        private Timer timeoutTimer;
+
+        /// <summary>
+        /// Called on ping from client. Int arguments is id process of client
+        /// </summary>
+        public event Action<ClientManager, Message> onPing;
+
+        /// <summary>
+        /// Called when client isn't responding. Int argument is id process of client
+        /// </summary>
+        public event Action<ClientManager> onTimeout;
+
+        public ClientManager(Config cfg)
+        {
+            port = cfg.port;
+            idleTime = cfg.idle;
+            executables = cfg.inputExec.ToList();
+            server = new IpcServer();
+
+            server.Start(port);
+            server.ReceivedRequest += MessageHandler;
+        }
+
+        /// <summary>
+        /// Create client-side process.
+        /// </summary>
+        /// <returns>If process was started successfully — returns true. If not — false</returns>
+        public bool Start()
+        {
+            var success = true;
+            try
+            {
+                var exeWithQuotes = executables.Select(exeWithArgs => "\"" + exeWithArgs + "\"");
+
+                var process = StartProcessAsCurrentUser(
+                    appPath: "app/IdleUserApp.exe",
+                    cmdLine: $"app/IdleUserApp.exe -i {idleTime} -p {port} -e {string.Join(" ", exeWithQuotes)}",
+                    visible: false);
+
+                clientID = (int)process.dwProcessId;
+                timeoutTimer = new Timer(Timeout, null, timeoutMS, 0);
+            }
+            catch (SessionUserTokenException)
+            {
+                logger.Info("Can't create client. Maybe IdleLoginService was executed not from SYSTEM user or there's no active user sessions right now");
+                clientID = null;
+                success = false;
+            }
+            return success;
+        }
+
+        /// <summary>
+        /// Kill client-side process
+        /// </summary>
+        public void Kill()
+        {
+            if (clientID != null)
+            {
+                var process = Process.GetProcessById((int)clientID);
+
+                // After client crash, another process can have same id as client previously
+                // So also check by process name.
+                // If it's client process name - kill it, else - do nothing
+                if (process.ProcessName == "IdleUserApp")
+                {
+                    process.Kill();
+                }
+            }
+        }
+
+        private void Timeout(object? state)
+        {
+            logger.Info($"Timeout from client id {clientID}");
+            onTimeout.Invoke(this);
+        }
+
+        private void MessageHandler(object sender, ReceivedRequestEventArgs e)
+        {
+            logger.Info(e.Request);
+            timeoutTimer?.Change(timeoutMS, 0);
+            onPing.Invoke(this, JsonConvert.DeserializeObject<Message>(e.Request));
+        }
+    }
+}
