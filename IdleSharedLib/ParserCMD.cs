1fe9a907e2cee5d16d2cb36fbefa24fb01165ce9
DareFox <easymalink@gmail.com>
2022-02-19 16:09:34 +0300

    Optimize imports

--- IdleSharedLib/ParserCMD.cs
+++ IdleSharedLib/ParserCMD.cs
@@ -1,10 +1,6 @@
 ﻿using CommandLine;
 using NLog;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IdleSharedLib
 {
